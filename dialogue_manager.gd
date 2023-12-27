0260e423d94e00f1cba03a0eef33c066dbf55403
Kyle <kyleisagod@gmail.com>
2023-12-27 16:06:29 -0500

    Added text advancing, textbxs disappears after dialogue

--- dialogue_manager.gd
+++ dialogue_manager.gd
@@ -4,8 +4,20 @@ extends Control
 @export var _avatar: TextureRect
 @export var _dialogue_resource: Dialogue
 
+var curr_slide_index := 0
 
 func _ready():
-	_dialogue_text.text = _dialogue_resource.dialogue_slides[0]
-	_avatar.texture = _dialogue_resource.avatar_texture
+	show_slide()
+
 	
+func _input(event):
+	if Input.is_action_just_pressed("advance_slide"):
+		if curr_slide_index < (_dialogue_resource.dialogue_slides.size() - 1):
+			curr_slide_index += 1
+			show_slide()
+		else:
+			visible = false
+
+func show_slide():
+	_dialogue_text.text = _dialogue_resource.dialogue_slides[curr_slide_index]
+	_avatar.texture = _dialogue_resource.avatar_texture
