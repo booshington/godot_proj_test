1927ee0798a9fb26b3e926436504e4305470b5df
Kyle <kyleisagod@gmail.com>
2023-12-27 15:51:34 -0500

    Completed path from resource object to various downstream consumers

--- dialogue_manager.gd
+++ dialogue_manager.gd
@@ -1,6 +1,11 @@
 extends Control
 
 @export var _dialogue_text: Label
+@export var _avatar: TextureRect
+@export var _dialogue_resource: Dialogue
+
 
 func _ready():
-	_dialogue_text.text = "welcome to the game, bro!"
+	_dialogue_text.text = _dialogue_resource.dialogue_slides[0]
+	_avatar.texture = _dialogue_resource.avatar_texture
+	
