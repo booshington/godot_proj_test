dfd3674d1a564e988d2e02636a4126fbb4775050
Kyle <kyleisagod@gmail.com>
2023-12-27 15:28:08 -0500

    Added dialogue text label field

--- dialogue_manager.gd
+++ dialogue_manager.gd
@@ -1,7 +1,6 @@
 extends Control
 
-@export var _dialogue_text: NodePath
+@export var _dialogue_text: Label
 
 func _ready():
-	var _dialogue_text: Label = get_node(_dialogue_text)
-	_dialogue_text.text = "welcome to the game!"
+	_dialogue_text.text = "welcome to the game, bro!"
